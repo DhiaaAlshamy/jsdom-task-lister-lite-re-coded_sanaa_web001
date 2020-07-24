58dc683f6a9241dcb643aa1c5bc4965632ca7ffa
Dhiaa Alshamy <dhodhof@gmail.com>
2020-07-24 18:48:26 +0300

    delete button is added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-task-form")
+  .addEventListener("submit",function(event){
+    addTasks();
+    event.preventDefault();
+  })
+  
+  
 });
+function addTasks(){
+  
+   let txt= document.getElementById("new-task-description").value;
+   let task=document.createElement("li");
+   let taskText=document.createElement("p");
+   let taskDeleteBtn=document.createElement("button");
+   taskDeleteBtn.textContent="Delete Task";
+   taskText.textContent=txt;
+   taskDeleteBtn.addEventListener("click",deleteItem)
+   taskText.appendChild(taskDeleteBtn);
+   task.className="task-item";
+   task.id=`${txt}`;
+  // taskText.innerHTML=`${txt}`;
+   task.appendChild(taskText)
+   document.getElementById("tasks").appendChild(task);
+ // document.getElementByName("deletebtn").addEventListener("click",deleteItem);
+}
+function deleteItem(element){
+  
+   element.toElement.parentElement.parentElement.remove();
+ }
+
